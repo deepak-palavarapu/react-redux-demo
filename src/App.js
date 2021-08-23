78443c685ef19cbdedd55961a3dac2ceb9ac22ef
DEEPAK PALAVARAPU <deepakpalavarapu@DEEPAKs-MacBook-Pro.local>
2021-08-23 21:12:12 +0530

    using hooks with redux in react app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import { Provider } from "react-redux";
 import store from './redux/store';
 import CakeContainer from './components/CakeContainer';
+import HookCakeContainer from './components/HookCakeContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Provider store={store}>
+          <HookCakeContainer></HookCakeContainer>
           <CakeContainer></CakeContainer>
         </Provider>
+
       </header>
     </div>
   );
