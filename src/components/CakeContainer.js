6bb331c908d5cfef4a8c989667df3eea07d5d831
DEEPAK PALAVARAPU <deepakpalavarapu@DEEPAKs-MacBook-Pro.local>
2021-08-23 20:55:51 +0530

    added redux in react app

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { buyCake } from '../redux/cakes/cakeActions'
+import { connect } from "react-redux";
+
+function CakeContainer(props) {
+    return (
+        <div>
+            <h1>Number of Cakes - {props.numOfCakes}</h1>
+            <button onClick={props.buyCake}>Buy cake</button>
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        numOfCakes: state.numOfCakes
+    }
+}
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CakeContainer)
